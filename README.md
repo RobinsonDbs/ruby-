622f3d961b9a99173e89912268f4c9e37d7b2442
Robinson <dubois.robinson@hotmail.fr>
2019-01-03 16:33:06 +0100

    exo 5 et modif README

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 <p>exo 2 : print ne va pas ajouter de nouvelle ligne, alors que puts va en ajouter à la fin de chaque argument si il n'en existe pas</p>
 <p>exo 3 : le # cache la 2ème ligne</p>
 <p>exo 4 : le programme ne se lance pas : "unterminated string meets end of file" car il manque le " finissant le string </p>
+<p>exo 5 : il est utilisé pour faire des calculs et afficher uniquement le résultat</p>
---
+++ exo_05.rb
@@ -0,0 +1,26 @@
+#affichera la phrase suivante
+puts "On va compter le nombre d'heures de travail à THP"
+#multiplie les données correspondant aux h de travail
+puts "Travail : #{10 * 5 * 11}"
+#multiplie les h pour les transformer en minutes
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+#affichera la phrase suivante
+puts "Et en secondes ?"
+#multiplie les données pour obtenir les secondes
+puts 10 * 5 * 11 * 60 * 60
+#affichera la phrase suivante
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+#vérifie l'équation 
+puts 3 + 2 < 5 - 7
+#affichera le calcul puis la réponse
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+#affichera le calcul puis la réponse
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+#affichera la phrase suivante
+puts "Ok, c'est faux alors !"
+#affichera la phrase suivante
+puts "C'est drôle ça, faisons-en plus :"
+#posera la question suivante puis vérifiera l'équation
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
