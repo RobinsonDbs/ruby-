bf63fd15ed99e59bec84b3941c1c6dc4a3a0e6bb
Robinson <dubois.robinson@hotmail.fr>
2019-01-03 18:41:51 +0100

    exo 8 et 9

---
+++ exo_08.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, quel est ton prénom?"
+user_name = gets.chomp
+print "Bonjour,"
+print user_name
+puts "!"
---
+++ exo_09.rb
@@ -0,0 +1,9 @@
+puts "Bonjour, quel est ton prénom?"
+user_name1 = gets.chomp
+puts "quel est ton nom?"
+user_name2 = gets.chomp
+print "Bonjour, "
+print user_name1
+print " "
+print user_name2
+puts " !"
