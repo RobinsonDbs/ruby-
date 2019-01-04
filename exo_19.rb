6e67b23ddc6f0861d0f5db80c4edb757c1ec2e2f
Robinson <dubois.robinson@hotmail.fr>
2019-01-04 23:35:51 +0100

    ex 19

--- exo_19.rb
+++ exo_19.rb
@@ -1,12 +1,21 @@
 collection = []
-number = 1
+	number = 1
+	
+	
+	while (number <= 50) do 
+		email = "jean.dupont.#{number}@mail.fr"
+	
+		number += 1
+		collection << email
+	end 
 
-while (number <= 50) do 
-	email = "jean.dupont.#{number}@mail.fr"
 
-	number += 1
-	collection << email
-end 
 
-puts "je n'ai pas réussit à sélectionner les paires, mon cerveau avait un peu de mal à fonctionner à la fin. Je me remets dessus ce week end"
-
+show_email = 1
+while show_email <= 50
+      if show_email % 2 == 0
+      
+        puts collection [show_email.to_i - 1]
+     end
+  show_email += 1
+end
